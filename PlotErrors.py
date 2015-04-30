fixing up the error calculation in the cdgTest also moifying how final plot is shown in the advection plots

PlotErrors.py
@@ -4,13 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #CFA my intersection routine
-#err=[0.852128,0.571278,0.335517]
+err=[0.852128,0.571278,0.335517]
 
 #CFA core intersection routine
 #err=[0.826784,0.553498,0.361314]
 
-#CDG constant basis funcs
-err=[0.736813,0.58143,0.604775]
+#CDG second order
+#err=[0.679821,0.498348,0.914343]
 dx =[1.0/32,1.0/64,1.0/128]
 
 lx = np.log(dx)

PlotFields.py
@@ -47,7 +47,7 @@ plt.colorbar()
 plt.savefig( 'ans' + '.' + '%.4u'%0 + '.png' )
 plt.figure()
 
-for i in np.arange( 0,tstep+tskip,tskip ):
+for i in np.arange( 0,tstep,tskip ):
 	print 'time step: ', i
 	phi = np.loadtxt( 'phi.' + '%.4u'%i + '.txt' )
 	plt.contourf( Xp, Yp, phi.reshape( ( len(yp), len(xp) ) ), 100 )
@@ -56,3 +56,11 @@ for i in np.arange( 0,tstep+tskip,tskip ):
 	plt.quiver( Xu[::8], Yu[::8], U[::8], V[::8] )
 	plt.savefig( 'phi.' + '%.4u'%i + '.png' )
 	plt.figure()
+
+phi = np.loadtxt( 'phi.' + '%.4u'%tstep + '.txt' )
+plt.contourf( Xp, Yp, phi.reshape( ( len(yp), len(xp) ) ), 100 )
+plt.clim( 0.0, 1.0 )
+plt.colorbar()
+plt.contour( Xp, Yp, ans.reshape( ( len(yp), len(xp) ) ) )
+plt.savefig( 'phi.' + '%.4u'%tstep + '.png' )
+plt.figure()