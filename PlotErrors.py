cbd84b9ed182194685d07ebb59a937256942c380
David Lee <drlee@pn1512444.local>
2015-05-11 16:26:53 -0600

    using new method to calculate the errors

--- PlotErrors.py
+++ PlotErrors.py
@@ -3,34 +3,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#CFA my intersection routine
-#err=[0.852128,0.571278,0.335517]
+#Q2B2, unlimited
+#err_l1=[0.0471054,0.0398877,0.084094]
+#err_l2=[0.0497258,0.0507822,0.107653]
 
-#CFA core intersection routine
-#err=[0.826784,0.553498,0.361314]
+#Q2B2, unlimited (unnormalized)
+#err_l1=[0.0036934,,]
+#err_l2=[0.00985396,,]
 
-#CDG second order (bill's limiter)
-#err=[0.679821,0.498348,0.914343]
+#Q3B2, unlimited
+#err_l1=[0.047951,0.0381468,0.0773436]
+#err_l2=[0.0498745,0.0501465,0.105948]
 
-#CDG second order (no limiter)
-#err=[0.286685,0.179829,0.178711]
-
-#CDG second order (kuzmin limiter)
-err=[0.680037,0.498419,0.914583]
-
-#CDG second order (venkatakrishnan limiter)
-#err=[1.38352,0.706516,0.962705]
-
-dx =[1.0/32,1.0/64,1.0/128]
+dx =[2.0/32,2.0/64,2.0/128]
 
 lx = np.log(dx)
-le = np.log(err)
+le = np.log(err_l1)
 
 y=np.vstack([lx,np.ones(len(lx))]).T
 m=np.linalg.lstsq(y,le)[0]
 print m
 
-plt.loglog( dx, err, '-o' )
+plt.loglog( dx, err_l1, '-o' )
 #plt.show()
 plt.xlabel( '$\Delta x$' )
 plt.ylabel( '$L_1 error$' )
