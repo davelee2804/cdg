fc3650f034aaccc674700769ce341cdc6835b751
David Lee <drlee@pn1512444.local>
2015-05-14 09:32:44 -0600

    plotting fields on the unstructured tri grid now

--- PlotFields.py
+++ PlotFields.py
@@ -3,64 +3,58 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.tri import *
 from pylab import *
+
+def analytic( x, y ):
+	xo = 0.5*np.cos( 0.75*np.pi )
+	yo = 0.5*np.sin( 0.75*np.pi )
+	r2 = (x - xo)*(x - xo) + (y - yo)*(y - yo)
+	if( np.sqrt( r2 ) ) < 0.4:
+		return exp( -40.0*r2 )
+	else:
+		return 0.0
+
+def basis_eval( x, y, p ):
+	return p[0] + p[1]*x + p[2]*y + p[3]*x*y
 
 tstep = int(sys.argv[1])
 tskip = int(sys.argv[2])
 
-xp = np.loadtxt( 'pgrid.x.txt' )
-yp = np.loadtxt( 'pgrid.y.txt' )
-xu = np.loadtxt( 'vgrid.x.txt' )
-yu = np.loadtxt( 'vgrid.y.txt' )
+XY = np.loadtxt( 'pgrid.txt' )
+p = XY[:,0]
+x = XY[:,1]
+y = XY[:,2]
 
-velx = np.loadtxt( 'velx' + '.' + '%.4u'%0 + '.txt' )
-vely = np.loadtxt( 'vely' + '.' + '%.4u'%0 + '.txt' )
+triang = Triangulation( x, y )
 
-velMag = np.sqrt( velx*velx + vely*vely )
+z = np.zeros( len(x) )
+a = np.zeros( len(x) )
 
-Xp,Yp = np.meshgrid( xp, yp )
-Xu,Yu = np.meshgrid( xu, yu )
-
-U = velx.reshape( ( len(yu), len(xu) ) )
-V = vely.reshape( ( len(yu), len(xu) ) )
-
-plt.contourf( Xu, Yu, velMag.reshape( ( len(yu), len(xu) ) ), 100 )
-plt.colorbar()
-plt.quiver( Xu[::8], Yu[::8], U[::8], V[::8] )
-plt.savefig( 'velMag' + '.' + '%.4u'%0 + '.png' )
-plt.figure()
-
-plt.contourf( Xu, Yu, U, 100 )
-plt.colorbar()
-plt.savefig( 'vel_x' + '.' + '%.4u'%0 + '.png' )
-plt.figure()
-
-plt.contourf( Xu, Yu, V, 100 )
-plt.colorbar()
-plt.savefig( 'vel_y' + '.' + '%.4u'%0 + '.png' )
-plt.figure()
-
-ans = np.loadtxt( 'ans' + '.' + '%.4u'%0 + '.txt' )
-plt.contourf( Xp, Yp, ans.reshape( ( len(yp), len(xp) ) ), 100 )
-plt.clim( 0.0, 1.0 )
-plt.colorbar()
-plt.savefig( 'ans' + '.' + '%.4u'%0 + '.png' )
-plt.figure()
-
-for i in np.arange( 0,tstep,tskip ):
+for i in np.arange( 0, tstep, tskip ):
 	print 'time step: ', i
-	phi = np.loadtxt( 'phi.' + '%.4u'%i + '.txt' )
-	plt.contourf( Xp, Yp, phi.reshape( ( len(yp), len(xp) ) ), 100 )
+	phi = np.loadtxt( 'phi_basis.' + '%.4u'%i + '.txt' )
+	for j in np.arange( len(x) ):
+		z[j] = basis_eval( x[j], y[j], phi[int(p[j])] )
+
+	plt.tricontourf( x, y, z, 100 )
 	plt.clim( 0.0, 1.0 )
 	plt.colorbar()
-	plt.quiver( Xu[::8], Yu[::8], U[::8], V[::8] )
 	plt.savefig( 'phi.' + '%.4u'%i + '.png' )
 	plt.figure()
 
-phi = np.loadtxt( 'phi.' + '%.4u'%tstep + '.txt' )
-plt.contourf( Xp, Yp, phi.reshape( ( len(yp), len(xp) ) ), 100 )
+for i in np.arange( len(x) ):
+	a[i] = analytic( x[i], y[i] )
+
+phi = np.loadtxt( 'phi_basis.' + '%.4u'%tstep + '.txt' )
+for j in np.arange( len(x) ):
+	z[j] = basis_eval( x[j], y[j], phi[int(p[j])] )
+
+plt.tricontourf( x, y, z, 100 )
 plt.clim( 0.0, 1.0 )
 plt.colorbar()
-plt.contour( Xp, Yp, ans.reshape( ( len(yp), len(xp) ) ) )
+plt.tricontour( x, y, a )
 plt.savefig( 'phi.' + '%.4u'%tstep + '.png' )
 plt.figure()
+
+
