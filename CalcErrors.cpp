15da965ef249720aaf287640147eb2c0f4d37f6c
David Lee <drlee@pn1512444.lanl.gov>
2015-05-19 12:34:02 -0600

    tidying up the calc errors test

--- CalcErrors.cpp
+++ CalcErrors.cpp
@@ -15,8 +15,8 @@
 
 using namespace std;
 
-#define QUAD_ORDER 3
-#define BASIS_ORDER 2
+#define QUAD_ORDER 6
+#define BASIS_ORDER 3
 
 double p1( double* p ) {
 	double	xo	= 0.5*cos( 0.75*M_PI );
@@ -24,19 +24,6 @@ double p1( double* p ) {
 	double	r2	= ( p[0] - xo )*( p[0] - xo ) + ( p[1] - yo )*( p[1] - yo );
 	if( sqrt( r2 ) < 0.40 ) return exp(-40.0*r2);
 	return 0.0;
-}
-
-double ErrorPerPoly( Field* phi, Func* f ) {
-	Grid*		grid	= phi->grid;
-	Polygon*	poly;
-	double		error 	= 0.0;
-	int			i;
-
-	for( i = 0; i < grid->nPolys; i++ ) {
-		poly = grid->polys[i];
-		error += grid->dx*grid->dy*fabs( phi->basis[i]->EvalFull( poly->origin ) - f( poly->origin ) );
-	}
-	return error;
 }
 
 void WriteDifference( Field* phi, Field* ans, int nx ) {
@@ -69,44 +56,26 @@ int main( int argc, char** argv ) {
 	int			nx, i;
 	Grid*		grid;
 	Field*		phi;
-	Field*		ans;
-	CDG*		cdg;
 	char		filename[80];
-	double		l1[3], l2[3], l1_norm[3], l2_norm[3], epp[3];
+	double		l1[3], l2[3], l1_norm[3], l2_norm[3];
 
 	i = 0;
-	for( nx = 32; nx < 256; nx *= 2 ) {
+	for( nx = 64; nx < 512; nx *= 2 ) {
 		cout << "nx: " << nx << endl;
 
-		grid = new Grid( nx, nx, -1.0, -1.0, +1.0, +1.0, QUAD_ORDER, BASIS_ORDER, true );
+		grid = new Grid( nx, nx, -2.0, -2.0, +2.0, +2.0, QUAD_ORDER, BASIS_ORDER, true );
 		phi	 = new Field( grid );
-		ans  = new Field( grid );
 
 		sprintf( filename, "input/Q%uB%u_basis_%.3u.txt", QUAD_ORDER, BASIS_ORDER, nx );
-		phi->ReadBasis( filename );
-
-		/* set the basis funcs for the  */
-		cdg = new CDG( ans, NULL, NULL, NULL, NULL );
-		cdg->InitBetaIJInv( p1 );
-		delete cdg;
-
-		WriteDifference( phi, ans, nx );
-
-		sprintf( filename, "phi_%.3u", nx );
-		phi->Write( filename, 256, 1 );
-
-		sprintf( filename, "pgrid_%.3u", nx );
-		grid->Write( filename, 1 );
+		phi->ReadBasis( filename, 0 );
 
 		l1_norm[i] = phi->L1Error( p1, true );
 		l2_norm[i] = phi->L2Error( p1, true );
 		l1[i] = phi->L1Error( p1, false );
 		l2[i] = phi->L2Error( p1, false );
-		epp[i] = ErrorPerPoly( phi, p1 );
 
 		i++;
 
-		delete ans;
 		delete phi;
 		delete grid;
 	}
@@ -125,10 +94,6 @@ int main( int argc, char** argv ) {
 
 	cout << "\nL_2 (normalized):    ";
 	for( i = 0; i < 3; i++ ) { cout << l2_norm[i] << "\t"; }
-	cout << endl;
-
-	cout << "\nerror per cell:      ";
-	for( i = 0; i < 3; i++ ) { cout << epp[i] << "\t"; }
 	cout << endl;
 
 	return 1;
