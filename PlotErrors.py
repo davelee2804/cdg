881347db29b807c5eb0942162f6f7bebeae1a819
dave <dave@dave-Dell-System-XPS-L322X>
2015-05-18 20:22:01 -0600

    removing error results for olg buggy model and adding errors for correct model, linear basis converges at > 2.0

--- PlotErrors.py
+++ PlotErrors.py
@@ -3,36 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Q2B2, unlimited
-#err_l1=[0.0471054,0.0398877,0.084094]
-#err_l2=[0.0497258,0.0507822,0.107653]
-
-#Q2B2, unlimited (unnormalized)
-#err_l1=[0.0036934,0.00312755,0.00659376]
-#err_l2=[0.00985396,0.0100633,0.0213332]
-
-#Q3B2, unlimited
-#err_l1=[0.047951,0.0381468,0.0773436]
-#err_l2=[0.0498745,0.0501465,0.105948]
-
 #Q3B2, unlimited (unnormalized)
-#err_l1=[0.00375985,0.00299105,0.00606446]
-#err_l2=[0.00988343,0.00993733,0.0209953]
-
-#Q3B2, unlimited (unnormalized)
-err_l1=[0.00333654,0.000665339,]
-#err_l2=[0.0078109,0.00146532,]
+#err_l1=[0.00333654,0.000665339,0.000172471]
+err_l2=[0.0078109,0.00146532,0.000336555]
 
 dx =[2.0/32,2.0/64,2.0/128]
 
 lx = np.log(dx)
-le = np.log(err_l1)
+le = np.log(err_l2)
 
 y=np.vstack([lx,np.ones(len(lx))]).T
 m=np.linalg.lstsq(y,le)[0]
 print m
 
-plt.loglog( dx, err_l1, '-o' )
+plt.loglog( dx, err_l2, '-o' )
 #plt.show()
 plt.xlabel( '$\Delta x$' )
 plt.ylabel( '$L_1 error$' )
