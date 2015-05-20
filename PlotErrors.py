ca8847569a3e5817bb5c1b3439ae340fb4a66928
David Lee <drlee@pn1512444.lanl.gov>
2015-05-20 16:11:35 -0600

    adding more points to the error estimate

--- PlotErrors.py
+++ PlotErrors.py
@@ -3,19 +3,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Q3B2, unlimited (unnormalized)
+#Q3B2 unlimited (unnormalized) (with cross terms)
 #err_l1=[0.00333654,0.000665339,0.000172471]
 #err_l2=[0.0078109,0.00146532,0.000336555]
 
-#Q3B2 new basis
+#Q3B2 unlimited (unnormalized)
 err_l1=[0.0052624,0.00094354,0.000210555]
 err_l2=[0.0118542,0.00210287,0.000403864]
+dx =[2.0/32,2.0/64,2.0/128]
+
+err_l1=[0.0104051,0.00190689,0.000376642,0.000103886]
+err_l2=[0.0222779,0.00439083,0.000767012,0.000201618]
+dx =[2.0/24,2.0/48,2.0/96,2.0/192]
 
 #Q6B3 new basis
 #err_l1=[0.000332518,8.33054e-05,3.73991e-05]
 #err_l2=[0.000609716,0.00016651,0.000117655]
 
-dx =[2.0/32,2.0/64,2.0/128]
 
 lx = np.log(dx)
 le = np.log(err_l1)
