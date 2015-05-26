73e1986840239cafb78ac5a0a013f87cfc94d08b
dave <dave@dave-Dell-System-XPS-L322X>
2015-05-25 20:47:44 -0600

    adding some addition errors for quad basis with smaller time step and rk4

--- PlotErrors.py
+++ PlotErrors.py
@@ -18,8 +18,15 @@ dx1 =[2.0/24,2.0/48,2.0/96,2.0/192]
 #cosine ic
 q4b2_l1=[0.000233686,3.18764e-05,4.43911e-06,9.90038e-07]
 q4b2_l2=[0.00063811,9.91304e-05,1.87187e-05,4.60721e-06]
+#cosine ic, half dt
+q4b2_l1_0p5dt=[0.000235419,3.71071e-05,4.44266e-06,6.61061e-07]
+q4b2_l2_0p5dt=[0.000641893,0.000114687,1.87089e-05,3.94163e-06]
+#q4b2_l1_rk4=[0.000233715,3.66977e-05,4.44281e-06,7.08988e-07]
+#q4b2_l2_rk4=[0.000638063,0.000113679,1.86816e-05,4.43494e-06]
+q4b2_l1_0p5dt_rk4=[0.000235427,3.71209e-05,4.45302e-06]
+q4b2_l2_0p5dt_rk4=[0.000641881,0.000114681,1.87076e-05]
 
-dx2 =[2.0/24,2.0/48,2.0/96,2.0/192]
+dx2 =[2.0/24,2.0/48,2.0/96]
 
 lx = np.log(dx1)
 le = np.log(q2b1_l1)
@@ -33,22 +40,36 @@ y=np.vstack([lx,np.ones(len(lx))]).T
 m2=np.linalg.lstsq(y,le)[0]
 print m2
 
-lx = np.log(dx2)
+lx = np.log(dx1)
 le = np.log(q4b2_l1)
 y=np.vstack([lx,np.ones(len(lx))]).T
 m3=np.linalg.lstsq(y,le)[0]
 print m3
 
-lx = np.log(dx2)
+lx = np.log(dx1)
 le = np.log(q4b2_l2)
 y=np.vstack([lx,np.ones(len(lx))]).T
 m4=np.linalg.lstsq(y,le)[0]
 print m4
 
+lx = np.log(dx2)
+le = np.log(q4b2_l1_0p5dt_rk4)
+y=np.vstack([lx,np.ones(len(lx))]).T
+m5=np.linalg.lstsq(y,le)[0]
+print m5
+
+lx = np.log(dx2)
+le = np.log(q4b2_l2_0p5dt_rk4)
+y=np.vstack([lx,np.ones(len(lx))]).T
+m6=np.linalg.lstsq(y,le)[0]
+print m6
+
 plt.loglog( dx1, q2b1_l1, '-o', label='Q2B1: $L_1 error=$'+str(m1[0])[:5] )
 plt.loglog( dx1, q2b1_l2, '-o', label='Q2B1: $L_2 error=$'+str(m2[0])[:5] )
-plt.loglog( dx2, q4b2_l1, '-o', label='Q4B2: $L_1 error=$'+str(m3[0])[:5] )
-plt.loglog( dx2, q4b2_l2, '-o', label='Q4B2: $L_2 error=$'+str(m4[0])[:5] )
+plt.loglog( dx1, q4b2_l1, '-o', label='Q4B2: $L_1 error=$'+str(m3[0])[:5] )
+plt.loglog( dx1, q4b2_l2, '-o', label='Q4B2: $L_2 error=$'+str(m4[0])[:5] )
+plt.loglog( dx2, q4b2_l1_0p5dt_rk4, '-o', label='Q4B2: $L_1 error (half dt)=$'+str(m5[0])[:5] )
+plt.loglog( dx2, q4b2_l2_0p5dt_rk4, '-o', label='Q4B2: $L_2 error (half dt)=$'+str(m6[0])[:5] )
 plt.legend( loc='upper left' )
 plt.title( 'Error convergence for the CDG advection scheme' )
 plt.xlabel( '$\Delta x$' )
