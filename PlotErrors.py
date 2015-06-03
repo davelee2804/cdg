2d892a73032478e5f32f3fd2cee8051260f64e83
dave <dave@dave-Dell-System-XPS-L322X>
2015-06-03 08:52:00 -0600

    adding errors for the constant basis funcs

--- PlotErrors.py
+++ PlotErrors.py
@@ -31,8 +31,6 @@ q4b2_l2_0p5dt=[0.000641893,0.000114687,1.87089e-05,3.94163e-06]
 q4b2_l1_0p5dt_rk4=[0.000235427,3.71209e-05,4.45302e-06,6.50418e-07]
 q4b2_l2_0p5dt_rk4=[0.000641881,0.000114681,1.87076e-05,3.92934e-06]
 
-dx2 =[2.0/24,2.0/48,2.0/96,2.0/192]
-
 #mpas fct, rk4
 fct_2nd=[0.0101906306466,0.00367051499661,0.0011991676866]
 fct_dx =[2.0/24,2.0/48,2.0/96]
@@ -44,9 +42,8 @@ lx = lx/lx[0]
 fct_dx = fct_dx*lx
 
 #cosine ic, half dt rk4
-q1b0_l1=[0.0554997,0.0378029,0.0230043]
-q1b0_l2=[0.144966,0.0990748,0.0600695]
-dx3    =[2.0/24,2.0/48,2.0/96]
+q1b0_l1=[0.0554997,0.0378029,0.0230043,0.0126504]
+q1b0_l2=[0.144966,0.0990748,0.0600695,0.0328107]
 
 lx = np.log(dx1)
 le = np.log(q2b1_l1)
@@ -60,25 +57,25 @@ y=np.vstack([lx,np.ones(len(lx))]).T
 m2=np.linalg.lstsq(y,le)[0]
 print m2
 
-lx = np.log(dx2)
+lx = np.log(dx1)
 le = np.log(q4b2_l1)
 y=np.vstack([lx,np.ones(len(lx))]).T
 m3=np.linalg.lstsq(y,le)[0]
 print m3
 
-lx = np.log(dx2)
+lx = np.log(dx1)
 le = np.log(q4b2_l2)
 y=np.vstack([lx,np.ones(len(lx))]).T
 m4=np.linalg.lstsq(y,le)[0]
 print m4
 
-lx = np.log(dx2)
+lx = np.log(dx1)
 le = np.log(q4b2_l1_0p5dt_rk4)
 y=np.vstack([lx,np.ones(len(lx))]).T
 m5=np.linalg.lstsq(y,le)[0]
 print m5
 
-lx = np.log(dx2)
+lx = np.log(dx1)
 le = np.log(q4b2_l2_0p5dt_rk4)
 y=np.vstack([lx,np.ones(len(lx))]).T
 m6=np.linalg.lstsq(y,le)[0]
@@ -90,19 +87,19 @@ y=np.vstack([lx,np.ones(len(lx))]).T
 m7=np.linalg.lstsq(y,le)[0]
 print m7
 
-lx = np.log(dx3)
+lx = np.log(dx1)
 le = np.log(q1b0_l1)
 y=np.vstack([lx,np.ones(len(lx))]).T
 m8=np.linalg.lstsq(y,le)[0]
 print m8
 
-plt.loglog( dx3, q1b0_l1, '-o', label='CDG, constant basis: m='+str(m8[0])[:5] )
+plt.loglog( dx1, q1b0_l1, '-o', label='CDG, constant basis: m='+str(m8[0])[:5] )
 plt.loglog( dx1, q2b1_l1, '-o', label='CDG, linear basis: m='+str(m1[0])[:5] )
 #plt.loglog( dx1, q2b1_l2, '-o', label='Q2B1: $L_2 error=$'+str(m2[0])[:5] )
 #plt.loglog( dx1, q4b2_l1, '-o', label='Q4B2: $L_1 error=$'+str(m3[0])[:5] )
 #plt.loglog( dx1, q4b2_l2, '-o', label='Q4B2: $L_2 error=$'+str(m4[0])[:5] )
-plt.loglog( dx2, q4b2_l1_0p5dt_rk4, '-o', label='CDG, quadratic basis: m='+str(m5[0])[:5] )
-#plt.loglog( dx2, q4b2_l2_0p5dt_rk4, '-o', label='Q4B2: $L_2 error (half dt)=$'+str(m6[0])[:5] )
+plt.loglog( dx1, q4b2_l1_0p5dt_rk4, '-o', label='CDG, quadratic basis: m='+str(m5[0])[:5] )
+#plt.loglog( dx1, q4b2_l2_0p5dt_rk4, '-o', label='Q4B2: $L_2 error (half dt)=$'+str(m6[0])[:5] )
 plt.loglog( fct_dx, fct_2nd, '-o', label='FCT, full correction: m='+str(m7[0])[:5] )
 plt.legend( loc='lower right' )
 plt.title( 'Error convergence for the CDG and FCT advection schemes' )
